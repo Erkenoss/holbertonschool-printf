ebbc687d53206b2b60458c90a4a8a60cdc98e511
Erkenoss <xavier.bertin6@gmail.com>
2023-07-26 07:54:23 -0700

    try fixed format NULL

--- printf_characters.c
+++ printf_characters.c
@@ -103,6 +103,9 @@ int _printf(const char *format, ...)
 	int total = 0, check = 0;
 	const char *_for = format;
 	va_list format_args;
+	
+	if (format == NULL)
+		return (-1);
 
 	va_start(format_args, format);
 
